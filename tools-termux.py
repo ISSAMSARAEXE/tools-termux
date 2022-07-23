fc7d90e1f7e537961cfbfac77518189e8a2e82d2
ISSAMSARAEXE <109853679+ISSAMSARAEXE@users.noreply.github.com>
2022-07-23 16:07:45 +0100

    Add files via upload

---
+++ tools-termux.py
@@ -0,0 +1,60 @@
+import os
+os.system('pkg install figlet')
+os.system('clear')
+v ='\033[0;35m'
+W  = '\33[0m'
+R  = '\33[1;31m'
+G  = '\33[1;32m'
+O  = '\33[1;33m'
+B  = '\33[1;34m'
+P  = '\33[1;35m'
+C  = '\33[1;36m'
+GR = '\33[1;37m'
+print(G)
+os.system('figlet      TOOLS ')
+print(R+'''
+	############################
+	# Install all termux tools #
+	# issam exe HACKER         #
+	############################''')
+print('''''')
+ox_r=input(B+'enter install tools termux [*]')
+print(GR)
+os.system('pkg update -y')
+os.system('pkg upgrade -y')
+os.system('pkg install python -y')
+os.system('pkg install python2 -y')
+os.system('pkg install python3 -y')
+os.system('pkg install ruby -y')
+os.system('pkg install git -y')
+os.system('pkg install php -y')
+os.system('pkg install java -y')
+os.system('pkg install google -y')
+os.system('pkg install bash -y')
+os.system('pkg install perl -y')
+os.system('pkg install nmap -y')
+os.system('pkg install clang -y')
+os.system('pkg install macchanger -y')
+os.system('pkg install nano -y')
+os.system('pkg install cowsay -y')
+os.system('pkg install curl -y')
+os.system('pkg install tar -y')
+os.system('pkg install zip -y')
+os.system('pkg install unzip -y')
+os.system('pkg install tor -y')
+os.system('pkg install sudo -y')
+os.system('pkg install wget -y')
+os.system('pkg install wcalc -y')
+os.system('pkg install openssl -y')
+os.system('pkg install bmon -y')
+os.system('clear')
+print(G+'''
+	######################################
+	# All the basics have been           #
+	# successfully installed             #
+	# sara exe issam exe hello everyone  #
+	######################################
+	''')
+ss_ii = input('enter exit tools-termux-$ ')
+os.system('exit')
+os.system('clear')
